61a8c68e1bff5ceb4aa6565f0c3b394c9b71cc58
Abdoul Abdou <abdoua@bu.edu>
2026-02-21 13:24:14 -0500

    This is identical to the ipynb rn

--- exp_max_pkmngo.py
+++ exp_max_pkmngo.py
@@ -1,14 +1,49 @@
-# import numpy as np
-# import random
-# import pandas as pd
+import numpy as np
+from scipy.stats import multivariate_normal
+import pandas as pd
+import json
 
-# from sklearn.mixture import GaussianMixture
+# constants
+EPSILON: float = 1e-12
+DELTA: float = 1e-9
 
-ct=0
-for i in range(1, 5):
-    for j in range(1, i+1):
-        for k in range(1, j+1):
-            for l in range(1, k+1):
-                ct+=l**1.005
+# round10k(num) rounds num to the nearest 10,000s place (up to 4 decimal places)
+def round10k(num):
+    return float(int(num*10000))/10000
 
-print(ct)
+
+# Load in JSON file
+filepath = "pokemon_data.json"
+with open(filepath, 'r') as file:
+    data = json.load(file)
+
+print(data)
+
+
+filepath = "pokemon_data.json"
+with open(filepath, 'r') as file:
+    data = json.load(file)
+x = 1
+for key in data:
+    print(x, key, str(data[key]["weight"])+" kg")
+    x+=1
+
+
+df = pd.DataFrame(data).T
+print(df)
+
+
+lightest = 0.25*df["weight"].min()
+heaviest = 2.5*df["weight"].max()
+shortest = 0.25*df["height"].min()
+tallest = 2.5*df["height"].max()
+
+rand = np.random.rand()
+
+print(f"The shortest Pokemon is {shortest} meters tall.")
+print(f"The tallest Pokemon is {tallest} meters tall.")
+print(f"The lightest Pokemon is {lightest} kg.")
+print(f"The heaviest Pokemon is {heaviest} kg.")
+
+val = rand*(heaviest-lightest)+lightest
+print(f"Sample weight: {round10k(val)} kg")
